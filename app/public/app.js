9b2bf3913832b0592d1e6fc05be124d93af9634d
guy eagelberg <eagelberg@gmail.com>
2015-06-24 20:54:58 +0300

    Merge branch 'master' of https://github.com/endaniel/AdvWebApp
    Conflicts:
    	.idea/workspace.xml
    	app/public/main.html

--- app/public/app.js
+++ app/public/app.js
@@ -1,5 +1,5 @@
 (function(){
-    var app = angular.module('app', ['ngRoute', 'ui.grid', 'd3', 'ui.grid.selection', 'ngAnimate', 'ngTouch','ngMaterial','restangular','angularFileUpload','angular-carousel','ui.select']);
+    var app = angular.module('app', ['ngRoute', 'ui.grid', 'd3', 'ui.grid.selection', 'ngAnimate', 'ngTouch','ngMaterial','restangular','angularFileUpload','angular-carousel','ui.select','screenApp']);
     app.config(function($routeProvider,$locationProvider){
         $locationProvider.html5Mode(true).hashPrefix('!');
         $routeProvider
